84fe6eb1a0b16be612754e18f9882e7ead4332b8
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-11-03 20:07:32 -0500

    (14.5.2) Value Arrays

--- src/value.h
+++ src/value.h
@@ -4,3 +4,13 @@
 #include "common.h"
 
 typedef double Value;
+
+typedef struct {
+	int capacity;
+	int count;
+	Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
