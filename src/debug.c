73ee7c268d2d874e9af192ffdb828744a0bfac91
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-11-03 20:23:09 -0500

    (14.5) Constant Instructions

--- src/debug.c
+++ src/debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "value.h"
 #include "debug.h"
 
 void disassembleChunk(Chunk* chunk, const char* name){
@@ -7,6 +8,13 @@ void disassembleChunk(Chunk* chunk, const char* name){
 	for (int offset = 0; offset < chunk->count;) {
 		offset = disassembleInstruction(chunk, offset);
 	}
+}
+static constantInstruction(const char* name, Chunk* chunk, int offset) {
+	uint8_t constant = chunk->code[offset + 1];
+	printf("%-16s %4d '", name, constant);
+	printValue(chunk->constants.values[constant]);
+	printf("'\n");
+	return offset + 2;
 }
 static int simpleInstruction(const char* name, int offset) {
 	printf("%s\n", name);
@@ -17,6 +25,8 @@ int disassembleInstruction(Chunk* chunk, int offset){
 	
 	uint8_t instruction = chunk->code[offset];
 	switch (instruction) {
+		case OP_CONSTANT:
+			return constantInstruction("OP_CONSTANT", chunk, offset);
 		case OP_RETURN:
 			return simpleInstruction("OP_RETURN", offset);
 		default:
