eb387045b338d7175bf4b4cbc7c6c92a8e257a80
Mahmoud Zeidan <qu.mahmoud.z@gmail.com>
2024-11-05 17:53:30 -0500

    (15.3) An Arithmetic Calculator

--- src/vm.c
+++ src/vm.c
@@ -22,20 +22,29 @@ static InterpretResult run() {
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+		printf("        ");
+		for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+			printf("[ ");
+			printValue(*slot);
+			printf(" ]");
+		}
+		printf("\n");
 		disassembleInstruction(vm.chunk,
 			(int)(vm.ip - vm.chunk->code));
 #endif
 		uint8_t instruction;
 		switch (instruction = READ_BYTE()) {
 			case OP_RETURN: {
+				printValue(pop());
+				printf("\n");
 				return INTERPRET_OK;
 			}
 			case OP_CONSTANT: {
 				Value constant = READ_CONSTANT();
-				printValue(constant);
-				printf("\n");
+				push(constant);
 				break;
 			}
+			case OP_NEGATE: push(-pop()); break;
 		}
 	}
 #undef READ_BYTE
