f8cbc9fd9d7549890e51db6136b25da8fdc0f7ec
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-11-10 20:25:33 -0500

    (16.3.2) Comments

--- src/scanner.c
+++ src/scanner.c
@@ -27,6 +27,10 @@ static char advance() {
 static char peek() {
 	return *scanner.current;
 }
+static char peekNext() {
+	if (isAtEnd()) return '\0';
+	return scanner.current[1];
+}
 static bool match(char expected) {
 	if (isAtEnd()) return false;
 	if (*scanner.current != expected) return false;
@@ -61,6 +65,15 @@ static void skipWhitespace() {
 		case '\n':
 			scanner.line++;
 			advance();
+			break;
+		case '/':
+			if (peekNext() == '/') {
+				// A comment goes until the end of the line.
+				while (peek() != '\n' && !isAtEnd()) advance();
+			}
+			else {
+				return;
+			}
 			break;
 		default:
 			return;
