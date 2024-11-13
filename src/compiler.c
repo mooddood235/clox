da1e82a0e3b414d811c8e7acc32cd193e5bf6ba4
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-11-12 20:59:54 -0500

    (17.4.1) Parsers for Tokens

--- src/compiler.c
+++ src/compiler.c
@@ -73,8 +73,26 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
 static void emitReturn() {
 	emitByte(OP_RETURN);
 }
+static uint8_t makeConstant(Value value) {
+	int constant = addConstant(currentChunk(), value);
+	if (constant > UINT8_MAX) {
+		error("Too many constants in one chunk.");
+		return 0;
+	}
+	return (uint8_t)constant;
+}
+static void emitConstant(Value value) {
+	emitBytes(OP_CONSTANT, makeConstant(value));
+}
 static void endCompiler() {
 	emitReturn();
+}
+static void number() {
+	double value = strtod(parser.previous.start, NULL);
+	emitConstant(value);
+}
+static void expression() {
+
 }
 bool compile(const char* source, Chunk* chunk){
 	initScanner(source);
