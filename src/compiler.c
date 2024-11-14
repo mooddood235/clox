9023153876d7cb8b4c251d5f2ff62fadc46ce30e
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-11-13 19:55:28 -0500

    (17.5) Parsing Infix Expressions

--- src/compiler.c
+++ src/compiler.c
@@ -101,6 +101,20 @@ static void emitConstant(Value value) {
 static void endCompiler() {
 	emitReturn();
 }
+static void binary() {
+	TokenType operatorType = parser.previous.type;
+	ParseRule* rule = getRule(operatorType);
+	parsePrecedence((Precedence)(rule->precedence + 1));
+
+	switch (operatorType) {
+		case TOKEN_PLUS: emitByte(OP_ADD); break;
+		case TOKEN_MINUS: emitByte(OP_SUBTRACT); break;
+		case TOKEN_STAR: emitByte(OP_MULTIPLY); break;
+		case TOKEN_SLASH: emitByte(OP_DIVIDE); break;
+		default: return; // Unreachable
+	}
+
+}
 static void grouping() {
 	expression();
 	consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression");
@@ -122,7 +136,7 @@ static void unary() {
 	}
 }
 static void parsePrecedence(Precedence precedence) {
-
+	
 }
 static void expression() {
 	parsePrecedence(PREC_ASSIGNMENT);
