d61d510e2c0b72b06fa894e5fbfda49457ad95f2
Mahmoud Zeidan <qu.mahmoud.z@gmail.com>
2024-12-20 14:35:17 -0500

    19.4 Operations on Strings

--- src/object.c
+++ src/object.c
@@ -7,7 +7,7 @@
 #include "vm.h"
 
 #define ALLOCATE_OBJ(type, objectType) \
-	(type*)allocateObject(sizeof(type), objectType);
+	(type*)allocateObject(sizeof(type), objectType)
 
 static Obj* allocateObject(size_t size, ObjType type) {
 	Obj* object = (Obj*)reallocate(NULL, 0, size);
@@ -27,4 +27,11 @@ ObjString* copyString(const char* chars, int length) {
 	memcpy(heapChars, chars, length);
 	heapChars[length] = '\0';
 	return allocateString(heapChars, length);
+}
+void printObject(Value value) {
+	switch (OBJ_TYPE(value)) {
+		case OBJ_STRING:
+			printf("%s", AS_CSTRING(value));
+			break;
+	}
 }
